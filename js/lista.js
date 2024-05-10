bfa960e3759246f378a6f44bcf8ca3dff3f3b4b2
Luana Vitória <100322178+luanabernardo@users.noreply.github.com>
2024-05-09 21:02:51 -0300

    Update lista.js

--- js/lista.js
+++ js/lista.js
@@ -1,11 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var cadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
+// lista.js
 
-    var listaCadastros = document.getElementById("listaCadastros");
+document.addEventListener("DOMContentLoaded", function() {
+    mostrarUsuarios();
+});
+
+function mostrarUsuarios() {
+    var cadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
+    var mainContent = document.getElementById("mainContent");
 
     cadastrados.forEach(function(usuario) {
-        var li = document.createElement("li");
-        li.textContent = "Nome: " + usuario.nome + ", Telefone: " + usuario.telefone;
-        listaCadastros.appendChild(li);
+        var cardDiv = document.createElement("div");
+        cardDiv.classList.add("card");
+
+        var cadastroDiv = document.createElement("div");
+        cadastroDiv.classList.add("cadastro");
+
+        var nomeP = document.createElement("p");
+        nomeP.innerHTML = "<strong>Nome:</strong> " + usuario.nome;
+
+        var cpfP = document.createElement("p");
+        cpfP.innerHTML = "<strong>CPF:</strong> " + usuario.cpf;
+
+        var telefoneP = document.createElement("p");
+        telefoneP.innerHTML = "<strong>Telefone:</strong> " + usuario.telefone;
+
+        var enderecoP = document.createElement("p");
+        enderecoP.innerHTML = "<strong>Endereço:</strong> " + usuario.endereco;
+
+        cadastroDiv.appendChild(nomeP);
+        cadastroDiv.appendChild(cpfP);
+        cadastroDiv.appendChild(telefoneP);
+        cadastroDiv.appendChild(enderecoP);
+
+        cardDiv.appendChild(cadastroDiv);
+        mainContent.appendChild(cardDiv);
     });
-});
+}
